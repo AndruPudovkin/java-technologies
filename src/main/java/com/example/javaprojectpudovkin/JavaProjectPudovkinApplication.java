feda80d38aa18f5ca622691aaa9aca6247b4c5fb
AndruPudovkin <pudowkin98@mail.ru>
2024-10-22 15:54:26 +0300

    initial commit

---
+++ src/main/java/com/example/javaprojectpudovkin/JavaProjectPudovkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprojectpudovkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectPudovkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectPudovkinApplication.class, args);
+    }
+
+}
